382700b3e92e0f48f43bcce0a1d0348abcbebc68
Eswara Sai <eswar.vankayalapati@gmail.com>
2016-12-06 19:19:11 +0530

    Fixed indentation issues

--- js/main.js
+++ js/main.js
@@ -1,232 +1,232 @@
 /* Developed by : Eswara Sai (http://eswarasai.com) */
 
-$(document).ready(function() {
-	function signupAnim() {
-		if ($(window).width() > 768) {
-			$('#account-form').animate({
-				'marginLeft' : "0"
-			});
+$(document).ready(function () {
+  function signupAnim() {
+    if ($(window).width() > 768) {
+      $('#account-form').animate({
+        'marginLeft': "0"
+      });
 
-			$('.account-main').animate({
-				'marginLeft' : "100%"
-			});
-		} else {
-			$('#account-form').animate({
-				'marginLeft' : "0"
-			});
+      $('.account-main').animate({
+        'marginLeft': "100%"
+      });
+    } else {
+      $('#account-form').animate({
+        'marginLeft': "0"
+      });
 
-			$('.account-main').animate({
-				'marginLeft' : "100%"
-			});
-		}
-	};
+      $('.account-main').animate({
+        'marginLeft': "100%"
+      });
+    }
+  };
 
-	function loginAnim() {
-		if ($(window).width() > 768) {
-			$('#account-form').animate({
-				'marginLeft' : "50%"
-			});
+  function loginAnim() {
+    if ($(window).width() > 768) {
+      $('#account-form').animate({
+        'marginLeft': "50%"
+      });
 
-			$('.account-main').animate({
-				'marginLeft' : "0"
-			});
-		} else {
-			$('#account-form').animate({
-				'marginLeft' : "0%"
-			});
+      $('.account-main').animate({
+        'marginLeft': "0"
+      });
+    } else {
+      $('#account-form').animate({
+        'marginLeft': "0%"
+      });
 
-			$('.account-main').animate({
-				'marginLeft' : "0"
-			});
-		}
-	};
+      $('.account-main').animate({
+        'marginLeft': "0"
+      });
+    }
+  };
 
-	$('#allowSignup').click(function(e) {
-		/* To prevent default event handling */
-		e.stopPropagation();
-		e.preventDefault();
-		e.stopImmediatePropagation();
+  $('#allowSignup').click(function (e) {
+    /* To prevent default event handling */
+    e.stopPropagation();
+    e.preventDefault();
+    e.stopImmediatePropagation();
 
-		signupAnim();
-	});
+    signupAnim();
+  });
 
-	$('#allowLogin').click(function(e) {
-		/* To prevent default event handling */
-		e.stopPropagation();
-		e.preventDefault();
-		e.stopImmediatePropagation();
+  $('#allowLogin').click(function (e) {
+    /* To prevent default event handling */
+    e.stopPropagation();
+    e.preventDefault();
+    e.stopImmediatePropagation();
 
-		loginAnim();
-	});
+    loginAnim();
+  });
 
-	$('#allowLoginFromReset').click(function(e) {
-		/* To prevent default event handling */
-		e.stopPropagation();
-		e.preventDefault();
-		e.stopImmediatePropagation();
+  $('#allowLoginFromReset').click(function (e) {
+    /* To prevent default event handling */
+    e.stopPropagation();
+    e.preventDefault();
+    e.stopImmediatePropagation();
 
-		loginAnim();
+    loginAnim();
 
-		$('.login-form').css('display', 'block');
-		$('.reset-form').css('display', 'none');
-	});
+    $('.login-form').css('display', 'block');
+    $('.reset-form').css('display', 'none');
+  });
 
-	$('#forgot').click(function(e) {
-		/* To prevent default event handling */
-		e.stopPropagation();
-		e.preventDefault();
-		e.stopImmediatePropagation();
+  $('#forgot').click(function (e) {
+    /* To prevent default event handling */
+    e.stopPropagation();
+    e.preventDefault();
+    e.stopImmediatePropagation();
 
-		$('.login-form').css('display', 'none');
-		$('.reset-form').css('display', 'block');
-	});
+    $('.login-form').css('display', 'none');
+    $('.reset-form').css('display', 'block');
+  });
 
-	/* Validation */
-	$("#loginForm").validate({
-		rules: {
-			loginEmail: {
-				required: true,
-				email: true
-			},
-			loginPassword: {
-				required: true,
-				minlength: 6
-			}
-		},
-        messages: {
-            loginEmail: {
-                required: "Email address is mandatory.",
-                email: "Please enter a valid email address."
-            },
-            loginPassword: {
-				required: "Please enter the password",
-				minlength: "Password should be atleast 6 characters."
-			},
-        },
-		errorClass: 'invalid',
-    	validClass: 'valid',
-        errorPlacement: function(error, element) {
-			$(element)
-            .closest("div")
-            .find("label[for=" + element.attr('id') + "]")
-            .attr('data-error', error.text());
-        }
-	});
+  /* Validation */
+  $("#loginForm").validate({
+    rules: {
+      loginEmail: {
+        required: true,
+        email: true
+      },
+      loginPassword: {
+        required: true,
+        minlength: 6
+      }
+    },
+    messages: {
+      loginEmail: {
+        required: "Email address is mandatory.",
+        email: "Please enter a valid email address."
+      },
+      loginPassword: {
+        required: "Please enter the password",
+        minlength: "Password should be atleast 6 characters."
+      },
+    },
+    errorClass: 'invalid',
+    validClass: 'valid',
+    errorPlacement: function (error, element) {
+      $(element)
+          .closest("div")
+          .find("label[for=" + element.attr('id') + "]")
+          .attr('data-error', error.text());
+    }
+  });
 
-	$("#signupForm").validate({
-		rules: {
-			signupFirstname: {
-				required: true
-			},
-			signupLastname: {
-				required: true
-			},
-			signupEmail: {
-				required: true,
-				email: true
-			},
-			signupPassword: {
-				required: true,
-				minlength: 6
-			},
-			signupConfirmPassword: {
-				required: true,
-				minlength: 6,
-				equalTo: '#signupPassword'
-			}
-		},
-        messages: {
-            signupEmail: {
-                required: "Please enter your email address",
-                email: "Please enter a valid email address"
-            },
-            signupPassword: {
-				required: "Please fill the password field",
-				minlength: "Password should be atleast 6 characters"
-			},
-			signupConfirmPassword: {
-				required: "Please fill the confirm password field",
-				minlength: "Confirm password should be atleast 6 characters",
-				equalTo: "Confirm password should be same as Password"
-			},
-        },
-		errorClass: 'invalid',
-    	validClass: 'valid',
-        errorPlacement: function(error, element) {
-			$(element)
-            .closest("div")
-            .find("label[for=" + element.attr('id') + "]")
-            .attr('data-error', error.text());
-        }
-	});
+  $("#signupForm").validate({
+    rules: {
+      signupFirstname: {
+        required: true
+      },
+      signupLastname: {
+        required: true
+      },
+      signupEmail: {
+        required: true,
+        email: true
+      },
+      signupPassword: {
+        required: true,
+        minlength: 6
+      },
+      signupConfirmPassword: {
+        required: true,
+        minlength: 6,
+        equalTo: '#signupPassword'
+      }
+    },
+    messages: {
+      signupEmail: {
+        required: "Please enter your email address",
+        email: "Please enter a valid email address"
+      },
+      signupPassword: {
+        required: "Please fill the password field",
+        minlength: "Password should be atleast 6 characters"
+      },
+      signupConfirmPassword: {
+        required: "Please fill the confirm password field",
+        minlength: "Confirm password should be atleast 6 characters",
+        equalTo: "Confirm password should be same as Password"
+      },
+    },
+    errorClass: 'invalid',
+    validClass: 'valid',
+    errorPlacement: function (error, element) {
+      $(element)
+          .closest("div")
+          .find("label[for=" + element.attr('id') + "]")
+          .attr('data-error', error.text());
+    }
+  });
 
-	$("#resetForm").validate({
-		rules: {
-			resetEmail: {
-				required: true,
-				email: true
-			}
-		},
-        messages: {
-            resetEmail: {
-                required: "Email address is mandatory.",
-                email: "Please enter a valid email address."
-            },
-        },
-		errorClass: 'invalid',
-    	validClass: 'valid',
-        errorPlacement: function(error, element) {
-			$(element)
-            .closest("div")
-            .find("label[for=" + element.attr('id') + "]")
-            .attr('data-error', error.text());
-        }
-	});
+  $("#resetForm").validate({
+    rules: {
+      resetEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      resetEmail: {
+        required: "Email address is mandatory.",
+        email: "Please enter a valid email address."
+      },
+    },
+    errorClass: 'invalid',
+    validClass: 'valid',
+    errorPlacement: function (error, element) {
+      $(element)
+          .closest("div")
+          .find("label[for=" + element.attr('id') + "]")
+          .attr('data-error', error.text());
+    }
+  });
 
 
-	/* Form submit actions */
-	$('#signup').click(function(e) {
-		/* To prevent default event handling */
-		e.stopPropagation();
-		e.preventDefault();
-		e.stopImmediatePropagation();
+  /* Form submit actions */
+  $('#signup').click(function (e) {
+    /* To prevent default event handling */
+    e.stopPropagation();
+    e.preventDefault();
+    e.stopImmediatePropagation();
 
-		if ($("#signupForm").valid()) {
-			swal("Success!", "You've registered successfully!", "success");
-		} else {
-			return false;
-		}
-	});
+    if ($("#signupForm").valid()) {
+      swal("Success!", "You've registered successfully!", "success");
+    } else {
+      return false;
+    }
+  });
 
-	$('#login').click(function(e) {
-		/* To prevent default event handling */
-		e.stopPropagation();
-		e.preventDefault();
-		e.stopImmediatePropagation();
+  $('#login').click(function (e) {
+    /* To prevent default event handling */
+    e.stopPropagation();
+    e.preventDefault();
+    e.stopImmediatePropagation();
 
-		if ($("#loginForm").valid()) {
-			swal("Success!", "You've logged in successfully!", "success");
-		} else {
-			return false;
-		}
-	});
+    if ($("#loginForm").valid()) {
+      swal("Success!", "You've logged in successfully!", "success");
+    } else {
+      return false;
+    }
+  });
 
-	$('#reset').click(function(e) {
-		/* To prevent default event handling */
-		e.stopPropagation();
-		e.preventDefault();
-		e.stopImmediatePropagation();
+  $('#reset').click(function (e) {
+    /* To prevent default event handling */
+    e.stopPropagation();
+    e.preventDefault();
+    e.stopImmediatePropagation();
 
-		if ($("#resetForm").valid()) {
-			$('#resetForm').css('display', 'none');
-			$('#reset').css('display', 'none');
-			$('#reset-message span').text('Password reset instructions has been successfully sent the provided email address. Please check the email for further instructions');
-			$('#reset-message').css('display', 'block');
-		} else {
-			return false;
-		}
-	});
+    if ($("#resetForm").valid()) {
+      $('#resetForm').css('display', 'none');
+      $('#reset').css('display', 'none');
+      $('#reset-message span').text('Password reset instructions has been successfully sent the provided email address. Please check the email for further instructions');
+      $('#reset-message').css('display', 'block');
+    } else {
+      return false;
+    }
+  });
 
-	$('#signup-carousel').carousel({full_width: true});
-	$('#login-carousel').carousel({full_width: true});
+  $('#signup-carousel').carousel({full_width: true});
+  $('#login-carousel').carousel({full_width: true});
 });
